c97f300025d791bcce7ac7b58dadf9c1dbd20bcc
DroneBreaker <brownierz01@gmail.com>
2023-06-07 22:34:28 +0100

    Added new feature to the menu page

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <div class="flex justify-around">
-            <div class="md:mx-16 mx-8 mt-24 bg-orange-500 md:h-[32rem] md:w-[22rem]">
+        <div class="md:flex md:justify-around">
+            <div class="md:mx-16 mx-8 mt-24 bg-orange-500 md:h-[32rem] h-[28rem] md:w-[22rem]">
                 <h1 class="md:mx-8 mx-4 uppercase text-4xl font-bold text-white mb-44 italic">Shawarma Haiven</h1>
 
                 <hr class="bg-white md:mx-8 mx-4 md:w-[12rem] mb-28" />
@@ -17,7 +17,7 @@
                 <h1 class="md:mx-8 mx-4 text-xs font-medium">Monday-Sunday, 10 AM - 10 PM</h1>
             </div>
 
-            <img class="md:mx-16 mx-8 md:h-[32rem] mt-24" src="../assets/menu-book.png" />
+            <img class="md:mx-16 mx-8 md:h-[32rem] h-[28rem] mt-24" src="../assets/menu-book.png" />
         </div>
     </div>
 </template>
